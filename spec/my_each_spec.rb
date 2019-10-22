215bbbfde9313196c08e6e7c91d6f216d7333554
Meagan Utsey <meagan.utsey@gmail.com>
2019-10-22 21:58:57 +0000

    Automatically backed up by Learn

--- spec/my_each_spec.rb
+++ spec/my_each_spec.rb
@@ -30,7 +30,7 @@ describe "my_each" do
     words = ['hi', 'hello', 'bye', 'goodbye']
     counter = 0
 
-    my_each(words) do |word|
+    my_each(words) do |item|
       expect(item).to equal(words[counter])
       counter+=1
     end
